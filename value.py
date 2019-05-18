c3c267284e439f0238e5a830d8dbe8f6b9fca4e0
Bushraafsar <50298017+Bushraafsar@users.noreply.github.com>
2019-05-18 12:42:23 -0700

    value of variable

---
+++ value.py
@@ -0,0 +1,5 @@
+# TASK NO 4"
+# PYTHON PROGRAM THAT COMPUTE THE VALUE OF (n+nn+nnn):
+n = int(input("PLEASE ENTER AN INTEGER NUMBER(n):"))
+VALUE = n + (n*n) + (n*n*n)
+print ("n + nn + nnn =",VALUE)
