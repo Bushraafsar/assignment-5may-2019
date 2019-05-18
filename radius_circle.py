e6ad8553677ac87fbf4a92e29e35ba9f4ff54d8d
Bushraafsar <50298017+Bushraafsar@users.noreply.github.com>
2019-05-18 12:39:16 -0700

    radius of circle

---
+++ radius_circle.py
@@ -0,0 +1,6 @@
+#TASK NO 1
+#PYTHON PROGRAM TO CALCULATE AREA OF CIRCLE BY ACCEPTING RADIUS BY USER!
+radius = int(input("please enter radius of cirlce = "))
+pi = 3.14
+area_of_circle = (pi*(radius**2))
+print("Area of Circle =",area_of_circle)
