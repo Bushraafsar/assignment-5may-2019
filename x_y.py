af0a99e0c76059a91da7eb2f45d7f7e8ddc6116e
Bushraafsar <50298017+Bushraafsar@users.noreply.github.com>
2019-05-18 12:47:21 -0700

    working with x and y

---
+++ x_y.py
@@ -0,0 +1,6 @@
+# task 14
+# python program to solve (x+y)*(x+y):
+x = int(input("ENTER VALUE OF (X):"))
+y = int(input("ENTER VALUE OF (Y):"))
+result = (x+y)*(x+y)
+print("the solution is :",result)
