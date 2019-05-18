e071c921e039bc8d3ff5ab0e861c0861bb56b5d9
Bushraafsar <50298017+Bushraafsar@users.noreply.github.com>
2019-05-18 12:37:55 -0700

    hyp or triangle

---
+++ hyp.py
@@ -0,0 +1,9 @@
+# TASK NO18
+# PYTHON PROGRAM TO COMPUTE HYPOTENUSE OF "TRIANGLE"!
+from math import sqrt
+a = float(input("the value of a:"))
+b= float(input("the value of b:"))
+a2 = a**2
+b2 = b**2
+c = sqrt(a2 + b2)
+print("hypotenuse is = ",c)
