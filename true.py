d68488b8075ede8c4fd2619c48bfcfd1951e4540
Bushraafsar <50298017+Bushraafsar@users.noreply.github.com>
2019-05-18 12:40:59 -0700

    checking of no.s whethere its is equal or not

---
+++ true.py
@@ -0,0 +1,9 @@
+# TASK NO 13
+# PYTHON PROGRAM THAT WILL RETURN TRUE IF TWO GIVEN NUMBERS ARE EQUAL OR THIER SUMS OR DIFFERENCE IS 5
+num1 = int(input("ENTER FIRST INTEGER:"))
+num2 = int(input("ENTER SECOND INTEGER:"))
+if num1 == num2 or num1-num2 == 5 or num1 + num2 == 5 :
+    print("TRUE!")
+else:
+    print("NOT TRUE!")    
+    
