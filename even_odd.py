9d266a13264f8a2d485af5027815c674ee25beeb
Bushraafsar <50298017+Bushraafsar@users.noreply.github.com>
2019-05-18 12:29:19 -0700

    even or odd number

---
+++ even_odd.py
@@ -0,0 +1,7 @@
+# TASK NO 10
+# PYTH0N PROGRAM TO CHECK WHETHER NO IS ODD OR EVEN!
+num = int(input("ENTER AN INTEGER NUMBER :"))
+if num % 2 == 0:
+    print("THE NUMBER IS EVEN!")
+else:
+    print("THE NUMBER IS ODD!")    
