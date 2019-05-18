3d259d17f56f7f258daaae060ee9ba493ff56e91
Bushraafsar <50298017+Bushraafsar@users.noreply.github.com>
2019-05-18 12:26:33 -0700

    difference of given no by 17

---
+++ difference.py
@@ -0,0 +1,6 @@
+#task no7
+#pythonprogram to get difference of between a number and  17.
+num = int(input("PLEASE ENTER A NUMBER :"))
+difference = num - 17
+result = abs(difference)
+print("THE RESULT IS =",result)
