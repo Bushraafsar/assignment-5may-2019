e048760b3f6d5c4a5dc4b622d7f7d99e95c2fbca
Bushraafsar <50298017+Bushraafsar@users.noreply.github.com>
2019-05-18 12:49:01 -0700

    vowel or consonant

---
+++ vowel_ornot.py
@@ -0,0 +1,7 @@
+# # TASK NO 11
+# PYTHON PROGRAM TO CHECK WHETER LETTER IS VOWEL OR NOT!
+letter = input("PLEASE ENTER A LETTER :")
+if letter == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u":
+    print ("IT IS VOWEL!")
+else:
+    print("IT IS NOT A VOWEL !, IT IS CONSONANT!!")    
