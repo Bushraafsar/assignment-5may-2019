66102445c4840bbe7ac94643680a210899e165d7
Bushraafsar <50298017+Bushraafsar@users.noreply.github.com>
2019-05-18 12:20:25 -0700

    copies of string

---
+++ copies.py
@@ -0,0 +1,6 @@
+# TASK NO 9
+# PYTHON PROGRAM TO GET STRING WHICH IS (N) NUMBER OF COPIES OF GIVEN STRING:
+string = input("ENTER A STRING HERE =")
+num = int(input("ENTER A NON NEGATIVE INTEGER ="))
+copies =(string )*num
+print("NO OF COPIES :", copies)
