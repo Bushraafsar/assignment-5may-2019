f4c338d6798abe5cf6a32bf82a9072686761dde3
Bushraafsar <50298017+Bushraafsar@users.noreply.github.com>
2019-05-20 09:49:37 -0700

    Conversion to seconds

---
+++ time_sec.py
@@ -0,0 +1,9 @@
+# TASK 20
+#PYTHON PROGRAM TO CONVERT ALL UNITS OF TIME TO SECONDS:
+time_hour = int(input("ENTER THE TIME IN HOURS:"))
+time_second2 = time_hour*60*60
+time_minutes = int(input("ENTER THE TIME IN MINUTES YOU WANT TO CONVERT IN SECONDS:"))
+times_second1 = time_minutes*60
+time = time_second2 + times_second1
+print('THE TIME IN SECONDS IS :',time)
+
