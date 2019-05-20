4440b481788371bd1109fd3f5996e175ab4e1624
Bushraafsar <50298017+Bushraafsar@users.noreply.github.com>
2019-05-20 22:26:50 +0500

    Include A E I O or U

--- vowel_ornot.py
+++ vowel_ornot.py
@@ -1,7 +1,7 @@
 # # TASK NO 11
 # PYTHON PROGRAM TO CHECK WHETER LETTER IS VOWEL OR NOT!
 character = input("PLEASE ENTER A LETTER :")
-if character == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u":
+if character == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u" or character =="A" or character =="E" or character ==  "I" or character == "O" or character == "U":
     print ("IT IS VOWEL!")
 else:
     print("IT IS NOT A VOWEL !, IT IS CONSONANT!!")    
