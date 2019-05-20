734349d098dc2f4a1d039ff50a743bb9dd45007e
Bushraafsar <50298017+Bushraafsar@users.noreply.github.com>
2019-05-20 09:46:34 -0700

    compute bmi

---
+++ bmi.py
@@ -0,0 +1,6 @@
+# TASK NO 22
+# PYTHON PROGRAM TO COMPUTE BODY MASS INDEX:
+height =  float(input("ENTER YOUR HEIGHT IN INCHES:"))
+weight = float(input("ENTER YOUR WEIGHT IN POUNDS:"))
+bmi = weight*703/height**2
+print("BODY MASS INDEX:",bmi)
