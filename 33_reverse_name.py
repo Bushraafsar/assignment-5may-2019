ee40414b61473436b1f6560d1c8684a6e449b8d4
Bushraafsar <50298017+Bushraafsar@users.noreply.github.com>
2019-05-24 09:11:51 -0700

    reverse first and last name with space

---
+++ 33_reverse_name.py
@@ -0,0 +1,8 @@
+# task no 33
+# python program to print user name in reverse order along with space:
+first_name = input("ENTER YOUR FIRST NAME:")
+last_name = input("ENTER YOUR LAST NAME:")
+first_name1 = first_name[::-1]
+last_name2 = last_name[::-1]
+print("USERNAME:",first_name1+" "+last_name2)
+   
