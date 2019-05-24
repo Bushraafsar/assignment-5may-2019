b3f8338413564368b62261cb4d335b60dd3c6c09
Bushraafsar <50298017+Bushraafsar@users.noreply.github.com>
2019-05-24 09:06:08 -0700

    sum first n integers

---
+++ 24_sum_frst_nint.py
@@ -0,0 +1,7 @@
+# TASK NO 24
+# PYTHOM PROGRAM TO CALCULATE SUM OF FIRST N POSITIVE INTEGER NUMBERS:
+n = int(input("ENTER AN INTEGER:"))
+sum1 = 0
+for i in range(1,n+1):
+    sum1 += i
+print("THE SUM OF FIRST N POSITIVE INTEGERS:",sum1)    
