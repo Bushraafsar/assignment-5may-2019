9107f3e68c3469f0bb905476fb2854bf91358db7
Bushraafsar <50298017+Bushraafsar@users.noreply.github.com>
2019-05-24 09:45:23 -0700

    multiplication table

---
+++ 39table_loop.py.py
@@ -0,0 +1,6 @@
+# task 39
+# PYTHON PROGRAM TO CREATE MULTIPLICATION TABLE:
+n = int(input("enter an integer:"))
+print("-------------------------MULTIPLICATION TABLE OF ",n,"------------------------")
+for i in range(1,11):
+    print(n ,"x",i,"=",i*n)
