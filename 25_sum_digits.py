926132412373f3bb8a13f1f20fe8dc7e1b3753b6
Bushraafsar <50298017+Bushraafsar@users.noreply.github.com>
2019-05-24 09:07:26 -0700

    sum of digits

---
+++ 25_sum_digits.py
@@ -0,0 +1,8 @@
+# TASK 25
+# PYTHON PRORAM TO CALCULATE SUM OF DIGITS OF N INTEGERS:
+n = input("ENTER AN INTEGER:")
+sum1=0
+for i in n:
+    sum1  +=  int(i)
+print("SUM OF DIGITS IS:",sum1)      
+
