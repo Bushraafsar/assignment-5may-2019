ba35266309618c80fd46377b850dd2cd031b1c57
Bushraafsar <50298017+Bushraafsar@users.noreply.github.com>
2019-05-28 09:00:23 -0700

    pattern by nested loop

---
+++ pattern_41_42_43.py
@@ -0,0 +1,36 @@
+#TASK41
+#PYTHON  PROGRAM TO  PRINT THE  PATTERN OF HALF  DAIMOND :
+print("========='HALF DAIMOND STAR PATTERN'==========")
+n = 5
+for  i in range(1,n+1):
+    for j in range(1,i+1):
+        print("*",end=" ") 
+    print()
+for i in range(n-1,0,-1) : 
+    for j in range(1,i+1):
+        print("*",end=" ") 
+    print()
+#TASK 42
+#PYTHON  PROGRAM TO  PRINT THE  PATTERN OF HALF  DAIMOND :
+print("========='HALF DAIMOND NUMBER PATTERN'==========")
+n = 5
+for  i in range(1,n+1):
+    for j in range(1,i+1):
+        print(j,end=" ") 
+    print()
+for i in range(n-1,0,-1) : 
+    for j in range(1,i+1):
+        print(j,end=" ") 
+    print()
+#TASK NO 43:
+# PYTHON PROGRAM TO PRINT NUMBER PATTERN BY USING NESTED FOR LOOP:
+print("==========='NUMBER PATTERN============='")
+n = 9
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(i,end=" ") 
+    print()    
+
+    
+
+    
